0e833b228ec1f02dcc7c5d1d2ad78e2c49aefe2c
junjie1475 <junjie1475@gmail.com>
2023-09-12 20:23:38 +0800

    Initial Commit

---
+++ iOS-microbench/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  iOS-microbench
+//
+//  Created by Junjie on 12/09/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS-microbench/iOS_microbenchApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_microbenchApp.swift
+//  iOS-microbench
+//
+//  Created by Junjie on 12/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct iOS_microbenchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
